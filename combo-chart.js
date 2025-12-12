6168fc77b16d637d068a3fd0739e01259f7916b7
Priya Rai <123924424+PriyaRai922@users.noreply.github.com>
2025-12-12 14:28:01 +0530

    Update combo-chart.js

--- combo-chart.js
+++ combo-chart.js
@@ -41,12 +41,11 @@
       //   line:   [ 22,  25,  18,  27,  24,  30]
       // };
 
-       this._demo = [{ x: 'Jan', net: 100, cogs: 50, gm: 50 }, 
-                          { x: 'Feb', net: 120, cogs: 55, gm: 75 }]; 
-            const cfg = { type: 'bar', data: { labels: ['Jan', 'Feb'], 
-            datasets: [{ label: 'Net sales', data: data, parsing: { yAxisKey: 'net' } }, 
-                       { label: 'Cost of goods sold', data: data, parsing: { yAxisKey: 'cogs' } }, 
-                       { label: 'Gross margin', data: data, parsing: { yAxisKey: 'gm' } }] }, }
+      this._demo = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        bar: [13.82, 12.16, 27.98, 24.90, 7.49, 28.30],
+        line: [131,   127,   94,    89,    68,   80   ]
+      };
 
 
       this._barLabel  = this.getAttribute("barlabel")  || "Revenue";
