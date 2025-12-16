0049cf9399e52579cfec9c900f9314f95a70cf62
Priya Rai <Priya.rai@percipere.co>
2025-12-16 13:41:39 +0530

    data_point

--- combo-chart.js
+++ combo-chart.js
@@ -106,7 +106,8 @@
       this._LabelData = { UniqueDate: uniqueDates };
       this._ProductListData = this._buildProductList(uniqueProducts);
     }
-
+     
+    // list of color
     _buildProductList(uniqueProducts) {
       const baseColors = [
         "#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd",
